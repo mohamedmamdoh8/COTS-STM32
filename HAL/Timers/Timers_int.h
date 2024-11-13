0230936018c25ce6e396d3c955db72c596c1d91f
mohamed mamdoh <124096379+mohamedmamdoh8@users.noreply.github.com>
2024-11-13 02:41:37 +0200

    Add files via upload

---
+++ HAL/Timers/Timers_int.h
@@ -0,0 +1,56 @@
+/*
+ * TIMERs_int.h
+ *
+ *  Created on: Dec 3, 2023
+ *      Author: philopater ghobrial
+ */
+
+#ifndef TIMERS_INT_H_
+#define TIMERS_INT_H_
+
+/*******************************************************************************
+ *                              Module Definitions                             *
+ ******************************************************************************/
+
+
+typedef struct {
+
+	    volatile u32 TIM1_CR1    ;
+	    volatile u32 TIM1_CR2    ;
+	    volatile u32 TIM1_SMCR   ;
+	    volatile u32 TIM1_DIER   ;
+	    volatile u32 TIM1_SR     ;
+	    volatile u32 TIM1_EGR    ;
+	    volatile u32 TIM1_CCMR1  ;
+	    volatile u32 TIM1_CCMR2  ;
+	    volatile u32 TIM1_CCER   ;
+	    volatile u32 TIM1_CNT    ;
+	    volatile u32 TIM1_PSC    ;
+	    volatile u32 TIM1_ARR    ;
+	    volatile u32 TIM1_RCR    ;
+	    volatile u32 TIM1_CCR1   ;
+	    volatile u32 TIM1_CCR2   ;
+	    volatile u32 TIM1_CCR3   ;
+	    volatile u32 TIM1_CCR4   ;
+	    volatile u32 TIM1_BDTR   ;
+	    volatile u32 TIM1_DCR    ;
+	    volatile u32 TIM1_DMAR   ;
+
+}TIM1_t;
+
+#define AUTO_RELOAD         0x0f
+
+#define PRESCALER           8
+
+#define REPETATION_COUNTER  0x0f
+
+/******************************************************************************
+ * modes
+ ******************************************************************************/
+#define COUNTERUP_MODE     1
+#define COUNTERDOWN_MODE   2
+
+void TIMER_vInit(void);
+
+
+#endif /* TIMERS_INT_H_ */
---
+++ HAL/Timers/Timers_prv.h
@@ -0,0 +1,19 @@
+/*
+ * Timers_prv.h
+ *   Created on: feb 16, 2024
+ *      Author: mohamed mamdoh
+ */
+
+#ifndef TIMERS_PRV_H
+#define TIMERS_PRV_H
+
+#define      TIM1       ((volatile TIM1_t*) 0x40010000)
+#define      TIM2       ((volatile TIMx_t*) 0x40000000)
+#define      TIM3       ((volatile TIMx_t*) 0x40000400)
+#define      TIM4       ((volatile TIMx_t*) 0x40000800)
+#define      TIM5       ((volatile TIMx_t*) 0x40000C00)
+#define      TIM9       ((volatile TIMx_t*) 0x40014000)
+#define      TIM10      ((volatile TIMx_t*) 0x40014400)
+#define      TIM11      ((volatile TIMx_t*) 0x40014800)
+
+#endif /* TIMERS_INT_H */
