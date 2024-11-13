3c7e4c386935ac52be372fd1d23dffd87c294a9c
mohamed mamdoh <124096379+mohamedmamdoh8@users.noreply.github.com>
2024-11-13 02:38:42 +0200

    Add files via upload

---
+++ HAL/DAC/DAC_int.h
@@ -0,0 +1,15 @@
+/*
+ * DAC_int.c
+ *
+ *  Created on: Sep 22, 2022
+ *      Author: Mohamed Mamdoh
+ */
+
+#ifndef DAC_INT_H
+#define DAC_INT_H
+
+void HDAC_Init (void);
+void HDAC_Song (void);
+void HDAC_Callback(void);
+
+#endif
